9113b70fe6432cb4441d7c21594f70b97c013a34
Rhema Michael <michael.rhema@gmail.com>
2023-10-19 12:23:18 +0000

    beg

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ import FormData from 'form-data'
 
 const app = express()
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 
 app.post('/', (req, res) => {
     handler(req, res)
