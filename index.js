1c47cb304543840ade3f79b017b29ee788da6d62
Rhema Michael <michael.rhema@gmail.com>
2023-10-19 14:23:58 +0000

    addin tct to voice

--- index.js
+++ index.js
@@ -16,11 +16,76 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.post('/', (req, res) => {
-    handler(req, res)
+app.post('/add-voice', (req, res) => {
+    addHandler(req, res)
 });
 
-async function handler(req, res) {
+
+app.post('/text-to-voice', (req, res) => {
+  t2vHandler(req, res)
+});
+
+
+async function t2vHandler(req, res) {
+
+  if (!process.env.ELEVENLABS_API_TOKEN) {
+    throw new Error(
+      "The ELEVENLABS_API_TOKEN environment variable is not set. See README.md for instructions on how to set it."
+    );
+  }
+  
+  try {
+    const apiToken = process.env.ELEVENLABS_API_TOKEN;
+    const { voice_id, text } = req.body
+    const url = `https://api.elevenlabs.io/v1/text-to-speech/${voice_id}`;
+    const headers = {
+      accept: 'audio/mpeg',
+      'xi-api-key': apiToken,
+      'Content-Type': 'application/json',
+    };
+    
+    const postData = {
+      text,
+      model_id: 'eleven_multilingual_v2',
+      voice_settings: {
+        stability: 0.5,
+        similarity_boost: 0.5,
+      },
+    };
+    
+    // const response = await axios.post(url, postData, {
+    //   headers
+    // });
+
+    const response = await axios({
+      method: 'post',
+      url,
+      data: postData,
+      headers,
+      responseType: 'stream' // Set the response type to 'stream' to handle large responses
+    })
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'audio/mpeg');
+    res.setHeader('Content-Disposition', 'attachment; filename="audio.mp3"');
+
+
+    // res.pipe(data);
+    response.data.pipe(res);
+
+
+  } catch (error) {
+    console.log(error?.response?.data);
+    res.statusCode = 500;
+    res.end(JSON.stringify(error?.response?.data));
+
+  }
+
+
+
+}
+
+async function addHandler(req, res) {
 
   if (!process.env.ELEVENLABS_API_TOKEN) {
     throw new Error(
