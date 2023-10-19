a660b1dde34b725f0f28dec6e28587120f67ab50
Rhema Michael <michael.rhema@gmail.com>
2023-10-19 11:38:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+import axios from 'axios';
+import http from 'http';
+import https from 'https';
+// @ts-ignore
+import got from 'got';
+
+const FormData = require('form-data')
+
+app.post('/', (req, res) => {
+    handler(req, res)
+});
+
+function handler(req, res) {
+
+  if (!process.env.ELEVENLABS_API_TOKEN) {
+    throw new Error(
+      "The ELEVENLABS_API_TOKEN environment variable is not set. See README.md for instructions on how to set it."
+    );
+  }
+  const apiToken = process.env.ELEVENLABS_API_TOKEN;
+
+  const { name, audio_file_url, description, labels } = req.body
+  let readStream: http.ClientRequest;
+  try {
+    // readStream = (audio_file_url.startsWith('https') ? https : http).get(audio_file_url);
+    readStream = got.stream(audio_file_url);
+  } catch (error) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: error }));
+  }
+
+  const form = new FormData();
+  form.append('name', name || 'Voice Name');
+  form.append('files', readStream, { filename: Date.now() + '-sample.mp3', contentType: 'audio/mpeg' });
+  form.append('description', description || 'Voice Description');
+  // form.append('labels', JSON.stringify(labels));
+
+
+  try {
+
+    const response = await axios.post('https://api.elevenlabs.io/v1/voices/add', form, {
+      headers: {
+        'accept': 'application/json',
+        'xi-api-key': apiToken,
+        ...form.getHeaders()
+      },
+    });
+
+    const data = response.data;
+    
+    res.statusCode = 200;
+    res.end(JSON.stringify(data));
+
+  } catch (error) {
+    console.log(error.response.data);
+    res.statusCode = 500;
+    res.end(JSON.stringify(error.response.data));
+
+  }
+
+
+
+}
 app.listen(process.env.PORT || 3000)
